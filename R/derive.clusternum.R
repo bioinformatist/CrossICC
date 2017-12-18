b24ee96af964b740ea7453d32d042157a12a95ef
Yu Sun <icannotendure666@gmail.com>
2017-12-18 10:58:24 +0800

    rename variables

--- R/derive.clusternum.R
+++ R/derive.clusternum.R
@@ -12,10 +12,10 @@ derive.clusternum <- function(consencus.result, cutoff = 0.05, maxK){
   for(k in 2:maxK){
     A=cal.auc(consencus.result,k)
     if(k == 2){
-      deltA=A
-      preA=A
-    } else deltA=(A-preA)/preA
-    if(deltA<=cutoff) return(k-1)
-    preA=A
+      ΔA=A
+      pre.A=A
+    } else ΔA=(A-pre.A)/pre.A
+    if(ΔA<=cutoff) return(k-1)
+    pre.A=A
   }
 }
