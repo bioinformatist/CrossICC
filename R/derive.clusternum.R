2a585370ec575181cc72d3fdc9e26e0e58ef4d4d
Yu Sun <icannotendure666@gmail.com>
2017-12-19 23:23:37 +0800

    formatting code

--- R/derive.clusternum.R
+++ R/derive.clusternum.R
@@ -10,12 +10,12 @@
 #' @examples
 derive.clusternum <- function(consencus.result, cutoff = 0.05, maxK){
   for(k in 2:maxK){
-    A=cal.auc(consencus.result,k)
+    A <- cal.auc(consencus.result,k)
     if(k == 2){
-      ΔA=A
-      pre.A=A
-    } else ΔA=(A-pre.A)/pre.A
-    if(ΔA<=cutoff) return(k-1)
-    pre.A=A
+      ΔA <- A
+      pre.A <- A
+    } else ΔA <- (A-pre.A) / pre.A
+    if(ΔA <= cutoff) return(k - 1)
+    pre.A <- A
   }
 }
