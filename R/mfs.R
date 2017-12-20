1699ee6c8ae6f4399f14dcfb6a110888be09ece2
Yu Sun <icannotendure666@gmail.com>
2017-12-20 22:38:07 +0800

    solve a Biiiiiiiiig problem

--- R/mfs.R
+++ R/mfs.R
@@ -1,5 +1,19 @@
 mfs <- function(x, y, z, filter.cutoff = 0.5, fdr.cutoff = 0.001){
-  invisible(sapply(list(x, y, z), function(x) setnames(x, 1, 'ID_REF')))
-  genes.com <- com.feature(x[,1], y[,1], z[,1], method = 'overlap')
-  merged <- MergeMaid::mergeExprs(as.matrix(subset(x, ID_REF %in% genes.com)[,-1]), as.matrix(subset(y, ID_REF %in% genes.com)[,-1]), as.matrix(subset(z, ID_REF %in% genes.com)[,-1]))
+
+  # Check if all input matrix is normal
+  invisible(lapply(list(x, y, z), check.eSet))
+
+  # TODO: Merge multiple probes for one gene here
+
+  genes.com <- com.feature(rownames(x), rownames(y), rownames(z), method="overlap")
+
+  # Must pre-assigned here (deep copy), or mergeExprs will raise error (seems its bug):
+  # Error in dimnames(x) <- dn :
+  #   length of 'dimnames' [2] not equal to array extent
+  x.genes.com <- x[genes.com,]
+  y.genes.com <- y[genes.com,]
+  z.genes.com <- z[genes.com,]
+
+  merged <- mergeExprs(x.genes.com,y.genes.com,z.genes.com)
+
 }
