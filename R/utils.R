e19a670ebebf0fc4e7e540bcc38dff213aa5ad20
Yu Sun <icannotendure666@gmail.com>
2017-12-21 17:21:33 +0800

    finish merge duplicates; add function for jitter

--- R/utils.R
+++ R/utils.R
@@ -11,18 +11,39 @@ check.eSet <- function(x){
   }
 }
 
-get.max.var.row <- function(x){
-  # Thanks to https://stackoverflow.com/users/3001626/david-arenburg
-  # at https://stackoverflow.com/a/25100036 for this line.
-  rowSums((x - rowMeans(x))^2)/(dim(x)[2] - 1)
-  # TODO: return row with maximum var, then embed it into below
+matrix.please <- function(x) {
+  # Thanks to https://stackoverflow.com/a/25695207
+  m<-as.matrix(x[,-1])
+  rownames(m)<-x[,1]
+  m
+}
+
+get.max.var.rows <- function(x){
+  DT <- data.table(row.names(x), x, var = apply(x, 1, var))
+  # Thanks to https://stackoverflow.com/a/29497254
+  DT <- setDT(DT)[, .SD[which.max(var)], by=V1]
+  m <- matrix.please(DT)
+  m[,-dim(m)[2]]
+}
+
+# Thanks to https://stackoverflow.com/a/4752580 for this function
+zero_range <- function(x, tol = .Machine$double.eps ^ 0.5) {
+  if (length(x) == 1) return(TRUE)
+  x <- range(x) / mean(x)
+  isTRUE(all.equal(x[1], x[2], tolerance = tol))
 }
 
 merge.duplicates <- function(x, method = "median") {
   switch(method,
-         "median" = aggregate(x, by=list(row.names(x)), FUN=median),
-         "mean" = aggregate(x, by=list(row.names(x)), FUN=median),
-         "max" = aggregate(x, by=list(row.names(x)), FUN=max),
-         "min" = aggregate(x, by=list(row.names(x)), FUN=min),
-         "variance" = )
+         # Thanks to https://support.bioconductor.org/u/3556/
+         # at https://support.bioconductor.org/p/41288/#41289 for this statement.
+         "median" = matrix.please(aggregate(x, by=list(row.names(x)), FUN = median)),
+         "mean" = matrix.please(aggregate(x, by=list(row.names(x)), FUN = mean)),
+         "max" = matrix.please(aggregate(x, by=list(row.names(x)), FUN = max)),
+         "min" = matrix.please(aggregate(x, by=list(row.names(x)), FUN = min)),
+         "var" = get.max.var.rows(x))
+}
+
+add.jitter <- function(x){
+  t(apply(x, 1, function(x) if(zero_range(x) == TRUE){jitter(x)}else{x}))
 }
