9222992c2209f9acbf03e131b0adc7ecf9f58e1c
Yu Sun <icannotendure666@gmail.com>
2017-12-22 22:00:23 +0800

    replace for loop with nested lapply

--- R/cor.cc.R
+++ R/cor.cc.R
@@ -1,5 +1,16 @@
-cor.cc <- function(x, y, z, cc, k){
-  platforms <- list(x = x, y = y, z = z)
-  lapply(names(k), function(p) lapply(1:k[[p]], function(q) apply(platforms[[p]][, names(which(cc[[p]][[k[[p]]]]$consensusClass == q)), with = FALSE], 1, median)))
-  shit <- as.data.table(unlist(fuck, recursive = FALSE))
+cor.cc <- function(xyz, cc, k){
+  centroids.list <- lapply(names(k),
+                           function(p) lapply(1:k[[p]],
+                                              function(q) apply(xyz[[p]][, names(which(cc[[p]][[k[[p]]]]$consensusClass == q))],
+                                                                1, median)))
+  centroids <- as.data.table(unlist(centroids.list,
+                                    recursive = FALSE))
+
+  centroids.names.list <- lapply(names(k),
+                                 function(x) lapply(1:k[[x]],
+                                                    function(y) paste(x, k[[x]], y, sep = '.')))
+  centroids.names <- unlist(centroids.names.list)
+  setnames(centroids, centroids.names)
+
+  cor(centroids, method="pearson")
 }
