d46c53c808446624fd040d866f9490dd3e77b7d1
Yu Sun <icannotendure666@gmail.com>
2017-12-23 18:48:26 +0800

    package meta function

---
+++ R/CrossICC.R
@@ -0,0 +1,12 @@
+#' CrossICC: Automatically Aggregating and Summarizing Bioinformatics Results for Interactive Report.
+#'
+#' The CrossICC package provides three categories of important functions:
+#' foo, bar and baz.
+#'
+#' @section CrossICC functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name CrossICC
+#' @import data.table
+NULL
