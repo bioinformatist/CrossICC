96ed12deb73f94418269e988c0732e6cfa10af04
Yu Sun <icannotendure666@gmail.com>
2017-12-24 15:55:13 +0800

    add ebayes

---
+++ R/ebayes.R
@@ -0,0 +1,28 @@
+ebayes <- function(eSet.subset, class, cutoff = 0.1){
+  k <- length(unique(class))
+  design <- model.matrix(~ 0 + factor(class))
+  colnames(design) <- paste("K", 1:k, sep = "")
+  K <- colnames(design)
+  fit <- limma::lmFit(eSet.subset, design)
+
+  x <- c()
+  for(i in 1:(k - 1)){
+    for(j in (i + 1):k){
+      x <- c(x, paste(K[j], K[i], sep = "-"))
+    }
+  }
+  for(i in 1:k){
+    x <- c(x, paste(K[i], paste("(", paste(K[-i], collapse="+"), ")", "/", k-1, sep=""), sep="-"))
+  }
+
+  contrast.matrix <- limma::makeContrasts(contrasts = x,levels = design)
+  fit2 <- limma::contrasts.fit(fit,contrast.matrix)
+  fit2 <- limma::eBayes(fit2)
+  gene.signature <- limma::topTable(fit2, number = 20000, adjust.method = 'BH', p.value = cutoff)
+  r <- list(gene.signature)
+  for(i in (k*(k - 1) / 2 + 1):(k * (k - 1) / 2 + k)){
+    geneset.i <- limma::topTable(fit2, number = 20000, coef = i, adjust.method = 'BH', p.value = cutoff)
+    r[[length(r)+1]] <- geneset.i
+  }
+  r
+}
