1691c8ab6f1ace21b855b1189eee0dbe922d198a
Yu Sun <icannotendure666@gmail.com>
2017-12-29 23:25:39 +0800

    keep on fighting with MergeMaid :)

--- R/m.f.s.R
+++ R/m.f.s.R
@@ -17,21 +17,21 @@ m.f.s <- function(platforms.list, filter.cutoff = 0.5, fdr.cutoff = 0.001){
   #   length of 'dimnames' [2] not equal to array extent
   # MergeMaid.R has been fine-adjusted in this package.
 
-  merged <- MergeMaid::mergeExprs(x.genes.com, y.genes.com, z.genes.com)
+  merged <- mergeExprs(genes.com.list)
 
-  tiff("gene.cor.matrix.tiff", compression = 'lzw', res = 300, width = 1600, height = 1600)
+  fig.size <- (length(platforms.list) + 1) * 400
+
+  tiff("gene.cor.matrix.tiff", compression = 'lzw', res = 300, width = fig.size, height = fig.size)
   null.ic <- unlist(MergeMaid::intcorDens(merged))
   dev.off()
 
-  pval.genes <- apply(as.matrix(rowMeans(MergeMaid::pairwise.cors(MergeMaid::intCor(merged, exact=FALSE)))), 1,
+  pval.genes <- apply(as.matrix(rowMeans(MergeMaid::pairwise.cors(intCor(merged, exact=FALSE)))), 1,
                       function(x){pval.cal(x, d=null.ic, alt="g")})
   fdr.genes <- p.adjust(pval.genes, method="fdr")
   genes.com.fdr <- names(which(fdr.genes < fdr.cutoff))
   filter.genes <- lapply(jittered, filter.mad, p = filter.cutoff)
 
-  filter.genes.com <- com.feature(filter.genes[[1]],
-                                  filter.genes[[2]],
-                                  filter.genes[[3]], method="overlap")
+  filter.genes.com <- com.feature(filter.genes, method="overlap")
 
   filter.scale <- lapply(jittered, function(x) scale(t(scale(t(x[filter.genes.com,])))))
 
