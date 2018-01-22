d59b79a08d5612c3ef1e990e7b0a78386244d1d0
likelet <zhaoqi699@sina.com>
2018-01-22 22:11:12 +0800

    add shiny frame

---
+++ inst/shiny/server.R
@@ -0,0 +1,18 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# add annotation
+#
+
+suppressMessages(library(shiny))
+suppressMessages(library(shinyjs))
+suppressMessages(library(heatmaply))
+suppressMessages(library(CrossICC))
+
+shinyServer(function(session,input, output) {
+
+
+
+
+})
