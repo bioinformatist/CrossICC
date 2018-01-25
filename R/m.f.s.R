9a3748429e983e8c602af89e659bf91f2504b0c2
likelet <zhaoqi699@sina.com>
2018-01-26 00:15:49 +0800

    set fdr cutoff to 1 to pass fake data test

--- R/m.f.s.R
+++ R/m.f.s.R
@@ -1,4 +1,4 @@
-m.f.s <- function(platforms.list, filter.cutoff = 0.5, fdr.cutoff = 0.001){
+m.f.s <- function(platforms.list, filter.cutoff = 0.5, fdr.cutoff = 1){
 
   # Merge multiple probes for one gene here
   non.duplicates <- lapply(platforms.list, merge.duplicates)
@@ -10,29 +10,33 @@ m.f.s <- function(platforms.list, filter.cutoff = 0.5, fdr.cutoff = 0.001){
 
   genes.com.list <- lapply(jittered, function(x) unlist(x)[genes.com,])
 
-  # Must pre-assigned here (deep copy, and must not be slice of list),
-  # or mergeExprs will raise error (exactly its bug):
-  # https://github.com/Bioconductor-mirror/MergeMaid/blob/baf0cfc0d370917d55c4b4adbc1d75c1141a3661/R/MergeMaid.R#L323
-  # Error in dimnames(x) <- dn :
-  #   length of 'dimnames' [2] not equal to array extent
-  # MergeMaid.R has been fine-adjusted in this package.
+  if (length(platforms.list) > 1) {
+    # Must pre-assigned here (deep copy, and must not be slice of list),
+    # or mergeExprs will raise error (exactly its bug):
+    # https://github.com/Bioconductor-mirror/MergeMaid/blob/baf0cfc0d370917d55c4b4adbc1d75c1141a3661/R/MergeMaid.R#L323
+    # Error in dimnames(x) <- dn :
+    #   length of 'dimnames' [2] not equal to array extent
+    # MergeMaid.R has been fine-adjusted in this package.
 
-  merged <- mergeExprs(genes.com.list)
+    merged <- mergeExprs(genes.com.list)
 
-  fig.size <- (length(platforms.list) + 1) * 400
+    fig.size <- (length(platforms.list) + 1) * 400
 
-  # tiff("gene.cor.matrix.tiff", compression = 'lzw', res = 300, width = fig.size, height = fig.size)
+    # tiff("gene.cor.matrix.tiff", compression = 'lzw', res = 300, width = fig.size, height = fig.size)
 
-  pdf(NULL)
-  dev.control('enable') # enable display list
-  null.ic <- unlist(MergeMaid::intcorDens(merged))
-  dev.off()
+    pdf(NULL)
+    dev.control('enable') # enable display list
+    null.ic <- unlist(MergeMaid::intcorDens(merged))
+    dev.off()
 
-  pval.genes <- apply(as.matrix(rowMeans(MergeMaid::pairwise.cors(intCor(merged, exact=FALSE)))), 1,
-                      function(x){pval.cal(x, d=null.ic, alt="g")})
-  fdr.genes <- p.adjust(pval.genes, method="fdr")
-  genes.com.fdr <- names(which(fdr.genes < fdr.cutoff))
-  filter.genes <- lapply(jittered, filter.mad, p = filter.cutoff)
+    pval.genes <- apply(as.matrix(rowMeans(MergeMaid::pairwise.cors(intCor(merged, exact=FALSE)))), 1,
+                        function(x){pval.cal(x, d=null.ic, alt="g")})
+    fdr.genes <- p.adjust(pval.genes, method = "fdr")
+    genes.com.fdr <- names(which(fdr.genes < fdr.cutoff))
+    filter.genes <- lapply(jittered, function(x) filter.mad(x[genes.com.fdr, ], p = filter.cutoff))
+  } else {
+    filter.genes <- lapply(jittered, function(x) filter.mad(x, p = filter.cutoff))
+  }
 
   filter.genes.com <- com.feature(filter.genes, method="overlap")
 
