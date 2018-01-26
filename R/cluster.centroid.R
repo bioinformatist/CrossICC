db832a937d9d140e2097ba07bfcdbab753509159
bioinformatist <icannotendure666@gmail.com>
2018-01-26 22:43:28 +0800

    reformat code

--- R/cluster.centroid.R
+++ R/cluster.centroid.R
@@ -1,12 +1,12 @@
 cluster.centroid <- function(x, gene.signature, cluster){
-  gene.signature <- intersect(rownames(x),gene.signature)
-  x.scale=t(scale(t(x[gene.signature,])))
-  centroids <-c()
+  # gene.signature <- intersect(rownames(x),gene.signature)
+  x.scale <- t(scale(t(x[gene.signature,])))
+  centroids <- c()
   for(i in unique(cluster)){
     centroids <- cbind(centroids,
                        apply(x.scale[gene.signature, names(which(cluster==i))], 1, median, na.rm=FALSE)
                        )
   }
-  colnames(centroids)=unique(cluster)
+  colnames(centroids) <- unique(cluster)
   return(centroids)
 }
