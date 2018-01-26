c0191d7eba214859b0ec769260c97fd08c3f34e8
bioinformatist <icannotendure666@gmail.com>
2018-01-26 11:03:06 +0800

    add fdr error; now push for update server

--- R/m.f.s.R
+++ R/m.f.s.R
@@ -1,4 +1,4 @@
-m.f.s <- function(platforms.list, filter.cutoff = 0.5, fdr.cutoff = 1){
+m.f.s <- function(platforms.list, filter.cutoff = 0.5, fdr.cutoff = 1) {
 
   # Merge multiple probes for one gene here
   non.duplicates <- lapply(platforms.list, merge.duplicates)
@@ -6,42 +6,51 @@ m.f.s <- function(platforms.list, filter.cutoff = 0.5, fdr.cutoff = 1){
   # Add jitter for extremly same expressed features through samples (if existed)
   jittered <- lapply(non.duplicates, add.jitter)
 
-  genes.com <- com.feature(lapply(jittered, rownames), method = 'overlap')
+  genes.com <- com.feature(lapply(jittered, rownames), method = "overlap")
 
-  genes.com.list <- lapply(jittered, function(x) unlist(x)[genes.com,])
+  genes.com.list <- lapply(jittered, function(x) unlist(x)[genes.com, ])
 
   if (length(platforms.list) > 1) {
-    # Must pre-assigned here (deep copy, and must not be slice of list),
-    # or mergeExprs will raise error (exactly its bug):
+    # Must pre-assigned here (deep copy, and must not be slice of list), or
+    # mergeExprs will raise error (exactly its bug):
     # https://github.com/Bioconductor-mirror/MergeMaid/blob/baf0cfc0d370917d55c4b4adbc1d75c1141a3661/R/MergeMaid.R#L323
-    # Error in dimnames(x) <- dn :
-    #   length of 'dimnames' [2] not equal to array extent
-    # MergeMaid.R has been fine-adjusted in this package.
+    # Error in dimnames(x) <- dn : length of 'dimnames' [2] not equal to array
+    # extent MergeMaid.R has been fine-adjusted in this package.
 
     merged <- mergeExprs(genes.com.list)
 
     fig.size <- (length(platforms.list) + 1) * 400
 
-    # tiff("gene.cor.matrix.tiff", compression = 'lzw', res = 300, width = fig.size, height = fig.size)
+    # tiff('gene.cor.matrix.tiff', compression = 'lzw', res = 300, width = fig.size,
+    # height = fig.size)
 
     pdf(NULL)
-    dev.control('enable') # enable display list
+    dev.control("enable")  # enable display list
     null.ic <- unlist(MergeMaid::intcorDens(merged))
     dev.off()
 
-    pval.genes <- apply(as.matrix(rowMeans(MergeMaid::pairwise.cors(intCor(merged, exact=FALSE)))), 1,
-                        function(x){pval.cal(x, d=null.ic, alt="g")})
+    pval.genes <- apply(as.matrix(rowMeans(MergeMaid::pairwise.cors(intCor(merged,
+      exact = FALSE)))), 1, function(x) {
+      pval.cal(x, d = null.ic, alt = "g")
+    })
     fdr.genes <- p.adjust(pval.genes, method = "fdr")
-    # TODO: dataset with fdr value over 0.1 should bring an error here
     genes.com.fdr <- names(which(fdr.genes < fdr.cutoff))
-    filter.genes <- lapply(jittered, function(x) filter.mad(x[genes.com.fdr, ], p = filter.cutoff))
+
+    if (length(genes.com.fdr) == 0) {
+      stop("The dataset has no common signature gene which can pass FDR filtering!\n
+           You may check if fake gene symbol/ID or wrongly annotated?")
+    }
+
+    filter.genes <- lapply(jittered, function(x) filter.mad(x[genes.com.fdr,
+      ], p = filter.cutoff))
   } else {
     filter.genes <- lapply(jittered, function(x) filter.mad(x, p = filter.cutoff))
   }
 
-  filter.genes.com <- com.feature(filter.genes, method="overlap")
+  filter.genes.com <- com.feature(filter.genes, method = "overlap")
 
-  filter.scale <- lapply(jittered, function(x) scale(t(scale(t(x[filter.genes.com,])))))
+  filter.scale <- lapply(jittered, function(x) scale(t(scale(t(x[filter.genes.com,
+    ])))))
 
   return(list(filtered.gene = filter.genes.com, filterd.scaled = filter.scale))
 }
