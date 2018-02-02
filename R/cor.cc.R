99c0423590347a853a08098b673d872aebab8f90
likelet <zhaoqi699@sina.com>
2018-02-02 17:15:10 +0800

    fix bug by apply

--- R/cor.cc.R
+++ R/cor.cc.R
@@ -1,7 +1,11 @@
 cor.cc <- function(xyz, cc, k, method = 'finer'){
+  # cat(xyz$Matrix.1, "\n")
+  # cat(names(k), k, "\n")
+  # cat(xyz$Matrix.1[, names(which(cc$Matrix.1[[5]]$consensusClass == 1))], "\n")
   centroids.list <- lapply(names(k),
                            function(p) lapply(1:k[[p]],
-                                              function(q) apply(xyz[[p]][, names(which(cc[[p]][[k[[p]]]]$consensusClass == q))],
+                                              # Thanks to https://stackoverflow.com/questions/28423275/dimx-must-have-a-positive-length-when-applying-function-in-data-frame/28423503#28423503
+                                              function(q) apply(xyz[[p]][, names(which(cc[[p]][[k[[p]]]]$consensusClass == q)), drop = FALSE],
                                                                 1, median)))
   centroids <- as.data.table(unlist(centroids.list,
                                     recursive = FALSE))
