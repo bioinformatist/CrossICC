44e768426c23489b929a6f9409baaa933bd4a729
likelet <zhaoqi699@sina.com>
2018-02-04 15:08:03 +0800

    add heatmap object to return value

--- R/ssGSEA.R
+++ R/ssGSEA.R
@@ -15,7 +15,7 @@
 #' CrossICC.object <- CrossICC(example.matrices, max.iter = 20)
 #' ssGSEA(example.matrices$x, CrossICC.object[[1]]$gene.signature, CrossICC.object[[1]]$unioned.genesets)
 #' }
-ssGSEA <- function(x, gene.signature, geneset2gene, plot.file = FALSE, color = c("purple", "#910202",
+ssGSEA <- function(x, gene.signature, geneset2gene, color = c("purple", "#910202",
   "#035005", "#1273d6", "#ff8207"), cluster) {
   # Our up-stream matrix is already with gene symbols, so provide fake
   # 'genewprobe' for runFAIME. But still need geneset2gene: An one-to-one mapping matrix with two columns,
@@ -30,21 +30,24 @@ ssGSEA <- function(x, gene.signature, geneset2gene, plot.file = FALSE, color = c
   fs.scale <- replace(fs.scale, fs.scale < -2, -2)
   fs.scale <- replace(fs.scale, fs.scale > 2, 2)
   sn <- gene.signature
-  if (plot.file) {
-    fs.scale <- fs.scale[, names(sort(cluster))]
-    col.col <- c()
-    cluster.member <- unique(sort(cluster))
-    for (i in 1:length(cluster)) {
-      cluster.names <- names(which(cluster == cluster.member[i]))
-      col.col <- c(col.col, rep(color[i], length(cluster.names)))
-    }
-    png(plot.file, width = 1000, height = 800, pointsize = 18)
-    if (require("gplots")) {
-      heatmap.2(fs.scale, col = greenred(75), ColSideColors = col.col, sepcol = "white",
-        sepwidth = c(0.001, 0.001), rowsep = 1:nrow(fs), scale = "none",
-        trace = "none", density.info = "none", labRow = NULL, labCol = NULL)
-    }
-    dev.off()
+
+  fs.scale <- fs.scale[, names(sort(cluster))]
+  col.col <- c()
+  cluster.member <- unique(sort(cluster))
+  for (i in 1:length(cluster)) {
+    cluster.names <- names(which(cluster == cluster.member[i]))
+    col.col <- c(col.col, rep(color[i], length(cluster.names)))
   }
-  return(fs)
+
+  pdf(NULL)
+  dev.control("enable")
+  if (require("gplots")) {
+    heatmap <- heatmap.2(fs.scale, col = greenred(75), ColSideColors = col.col, sepcol = "white",
+      sepwidth = c(0.001, 0.001), rowsep = 1:nrow(fs), scale = "none",
+      trace = "none", density.info = "none", labRow = NULL, labCol = NULL)
+  }
+
+  dev.off()
+
+  return(list(fs, heatmap))
 }
