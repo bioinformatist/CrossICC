acff08436dcb28c9d8a95123e768c46d1c833508
likelet <zhaoqi699@sina.com>
2018-05-10 15:40:00 +0800

    add run shiny command

---
+++ R/runshiny.R
@@ -0,0 +1,8 @@
+#' Run the default shiny report independently
+#'
+#' \code{runShinyCrossICC} run shiny independently
+#' @author Qi Zhao
+#' @export
+runShinyCrossICC<-function(){
+  shiny::runApp(system.file("shiny", package = "CrossICC"))
+}
