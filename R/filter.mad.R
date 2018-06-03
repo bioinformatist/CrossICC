3c973af3787144f38671445f8aef767d089b4130
Yu Sun <icannotendure666@gmail.com>
2018-06-03 21:09:49 +0800

    move two function into m.f.s

--- R/filter.mad.R
+++
@@ -1,8 +0,0 @@
-filter.mad <- function(x, p = 0.5, method = 'percent'){
-  x.mad <- apply(x, 1, function(xr) mad(xr[!is.na(xr)]))
-  x.mad.rank <- rank(-x.mad)
-  switch (method,
-    "percent" = names(x.mad.rank[x.mad.rank < p * length(x.mad)]),
-    "absolute" = names(x.mad[x.mad > p])
-  )
-}
