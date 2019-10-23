83bb2fe1db4502ec118efc28aef0fe04390df191
Yu Sun <icannotendure666@gmail.com>
2019-10-23 16:48:27 +0800

    fix problem in test; v1.0.0

--- tests/testthat/test-crossicc.R
+++ tests/testthat/test-crossicc.R
@@ -1,7 +1,7 @@
 context("test-crossicc")
 
 data("demo.platforms")
-CrossICC.obj <- CrossICC(demo.platforms, skip.mfs = TRUE, max.iter = 100, use.shiny = FALSE, cross = "cluster", overwrite = TRUE)
+CrossICC.obj <- CrossICC(demo.platforms, skip.mfs = TRUE, max.iter = 100, use.shiny = FALSE, cross = "cluster", overwrite = TRUE, output.dir = tempdir())
 test_that("CrossICC works", {
   expect_equal(length(CrossICC.obj), 10)
 })
